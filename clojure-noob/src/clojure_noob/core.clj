6ccf4b6f539ecc574f82e7758ae8f612a5bfe0ff
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2014-12-04 01:17:25 -0500

    some experiments

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -5,3 +5,13 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "I'm a little teapot!"))
+
+(defn params
+  ([x]
+   (str "I take one param: " x " is it a string?"))
+  ([x y]
+   (str "I take two params: " x y " are they strings?")))
+
+(defn tell-x-y
+  [{x :x y :y}]
+  (list x y))
